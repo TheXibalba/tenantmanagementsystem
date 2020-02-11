2d15d794b8fa0b1c99964eaa54e95a45d82bd950
Yuvraj Pawar <yurajpawar99@gmail.com>
2020-02-11 22:07:37 +0530

    worked out the bill logic. Work out the rent calulation logic. Prefer the calculation to be at the DB side instead of the front end

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ const tmsAdminSchema=new mongoose.Schema({
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
-app.use(express.static("public"));
+app.use(express.static(__dirname+"/public"));
 
 var date = new Date();
 var dateStr = date.toString();
@@ -48,44 +48,30 @@ app.get("/", function (req, res) {
 
 });
 
+let embedRent={
+ month:String,
+ 
+ paidAmount:Number,
+ pendingAmount:{
+     type:Number,
+   default:0
+ },
 
-app.get("/tenantRent", function (req, res) {
-    res.render("tenantRent", {
-        tenantID: "01",
-        tenantName: "some Tenant",
-        flatType: "Some flat",
-        rentAmount: 1000,
-        month: "some month"
-    });
-});
-app.get("/tenantBill", function (req, res) {
-    res.render("tenantBill", {
-        tenantID: "01",
-        tenantName: "some Tenant",
-        billAmount: 1000,
-        month: justMonth
-    });
+ rentPaymentStatus:String
 
-});
-app.get("/tenantBillFull", function (req, res) {
-    res.render("tenantBillFull", {
-        tenantID: "01",
-        tenantName: "some Tenant",
-        month: "Some Month",
-        prevREjs: 1000,
-        currREjs: 2000,
-        rateEjs: 10.22,
-        dateEjs: "some date",
-        billOfMonth: 1212,
-        paymentStatus: "Don't know"
+};
 
-    });
-});
+let embedBill={
 
-app.get("/tenantAddTable",function(req,res){ 
-    res.render("tenantAddTable");
+    month:String,
+    prevReading:Number,
+    currReading:Number,
+    rpu:Number,
+    takenOnDate:String,
+    totalBill: Number,
+    billPaymentStatus:String
 
-});
+}  ;
 
 const tmsTenantSchema=new mongoose.Schema({
     flatId:Number,
@@ -94,10 +80,98 @@ const tmsTenantSchema=new mongoose.Schema({
     contact:Number,
     startingDate:Date,
     agreedUponRent:Number,
+
+    rent:[embedRent],
+    bill:[embedBill]
     });
 
     const Tenant=new mongoose.model("tenant",tmsTenantSchema);
 
+
+app.get("/tenantRent/:ID", function (req, res) {
+    const c=req.params.ID;
+    
+    Tenant.findOne({flatId:c},function(err,data){
+        if(err){
+            console.log("Error locating rent details!");
+        }
+        else{
+
+            let k=0;
+                Tenant.findOne({flatId:c},function(err,data1){
+                    if(data1.rent.length!=0){
+                     
+                        k=Number(data1.rent[data1.rent.length-1].pendingAmount);
+
+                    }
+                 
+                 
+               
+                console.log("total backlog: "+k);
+                
+                 
+                 res.render("tenantRent", {
+                tenantName:data.name,
+                tenantID: data.flatId,
+                receivedData:data.rent,
+                fixedRent:data1.agreedUponRent,
+                paidAmount:data.rent[1],
+                pending: k
+                
+
+
+
+                });
+                    });
+           
+
+           
+
+
+            }
+
+
+    });
+    
+    
+    
+  
+});
+
+
+app.post("/tenantRent/:ID",function(req,res){
+const c=req.params.ID;
+
+let insertRent={
+month: req.body.rentMonth,
+paidAmount:req.body.paidAmount,
+pendingAmount: req.body.pendingRent,
+rentPaymentStatus: req.body.paymentStatus
+};
+
+
+Tenant.updateOne({flatId:c},{$push:{rent:insertRent}},function(err,data){
+    if(err){
+        console.log("error updating rent details!");
+    }else{
+        console.log("Successfully updated the rent data! "+data);
+    }
+});
+
+
+console.log(req.body);
+res.redirect(`/tenantRent/${c}`);
+
+});
+
+
+
+app.get("/tenantAddTable",function(req,res){ 
+    res.render("tenantAddTable");
+
+});
+
+    
 
 app.post("/tenantAddTable",function(req,res){
     /* database commands go here */
@@ -107,6 +181,7 @@ app.post("/tenantAddTable",function(req,res){
     let agreedRent;
     let ID;
     let flatAllocationDate;
+   
     
     ID=req.body.ID;
     newUser=req.body.newTenantName;
@@ -135,9 +210,22 @@ app.post("/tenantAddTable",function(req,res){
            if(err){
                console.log(err);
            }else{
-               console.log("Saved successfully!: \n"+tenant);
+               console.log("Saved successfully!: \n");
            }
        });
+       const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December" ];
+  const d = new Date();
+  
+                                                                   
+       letRentFirstObj={
+           month: monthNames[d.getMonth()],
+           paidAmount:agreedRent,
+           pendingAmount:0,
+           rentPaymentStatus: "PAID"
+       };
+
+       Tenant.updateOne({flatId:ID},{$push:{rent:rentFIrstObj}});
 
  
    res.redirect("/allUsers");
@@ -146,10 +234,6 @@ app.post("/tenantAddTable",function(req,res){
 
 
 
-var previousReading;
-var currentReading;
-var ratePerUnit;
-var finalBill;
 
 
 app.post("/", function (req, res) {
@@ -182,27 +266,54 @@ app.get("/allUsers", function (req, res) {
             console.log(err);
         }else{
            /*  console.log(data); */
-            res.render("allUsers",{ID:101,receivedData:data});
+            res.render("allUsers",{receivedData:data});
         }
     });
    
 });
 
 
-app.post("/tenantBill", function (req, res) {
-    console.log(req.body);
-    previousReading = Number(req.body.prevR);
-    currentReading = Number(req.body.currR);
-    ratePerUnit = Number(req.body.rate);
-    finalBill = (currentReading - previousReading) * ratePerUnit;
+app.post("/tenantBillFull/:ID", function (req, res) {
+    const c=(Number)(req.params.ID);
+    /* console.log(req.body); */
+    let billMonth= req.body.billingMonth;
+    let previousReading = Number(req.body.prevR);
+    let currentReading = Number(req.body.currR);
+    let ratePerUnit = Number(req.body.rate);
+    let takenOn=req.body.takenOn;
+    let finalBill = (currentReading - previousReading) * ratePerUnit;
+    let status=req.body.paymentStatus;
+//MongoDB bill saving commands
 
+const insertBill={
+    month:billMonth,
+    prevReading:previousReading,
+    currReading:currentReading,
+    rpu:ratePerUnit,
+    takenOnDate: takenOn,
+    totalBill: finalBill,
+    billPaymentStatus: status
+};
+
+
+Tenant.updateOne({flatId:c},{$push:{bill:insertBill}},function(err,data){
+    if(err){
+        console.log("error during updation of bill!");
+    }else{
+        console.log("bill successfully updated!");
+    }
+    res.redirect("/tenantBillFull/"+c);
+}
+ 
+
+);
 });
 
 
 
 app.get("/delete/:contactNo",function(req,res){
     const c=req.params.contactNo;
-    console.log(c);
+    /* console.log(c); */
 Tenant.deleteOne({contact:c },function(err){
     if(err){
         console.log(err);
@@ -212,6 +323,34 @@ Tenant.deleteOne({contact:c },function(err){
     res.redirect("/allUsers");
 });
 });
+
+app.get("/tenantBillFull/:flatIDEjs", function (req, res) {
+    
+    const c2=(req.params.flatIDEjs);
+  /*   console.log("data type of C2: "+typeof(c2)); */
+    Tenant.findOne({flatId:c2},function(err,data){
+        if(err){
+            console.log(err);
+        }
+        else{
+           
+            /* console.log("THE VALUE OF C2: "+c2); */
+           /*  console.log(data); */
+             
+            res.render("tenantBillFull", {
+            name:data.name,
+            tenantID:data.flatId,
+            receivedData:data
+
+           } );
+        }
+    });
+    
+    
+   
+});
+
+
 
 app.listen(3000, function () {
     console.log("Server started on port 3000");
--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+//jshint esversion:6
+
 console.log("javascript is running fine!");
 
 
@@ -6,3 +8,35 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip();
   });
 
+
+  let fixedRent=document.getElementById("fixedRent");
+  let paidAmount=document.getElementById("paidAmount");
+  let pendingRent=document.getElementById("pendingRent");
+  let pa=document.getElementById("pa").lastChild;
+  let total=0;
+  let flag=0;
+
+  console.log(fixedRent,paidAmount,pendingRent);
+
+
+   
+  paidAmount.addEventListener("change",function(event){
+    
+  /*  if(Number(paidAmount.value)>(Number()+Number())) */
+
+
+
+     pendingRent.value= Number(fixedRent.value)-Number(paidAmount.value);
+      
+     
+   
+  });
+
+
+  
+   total=Number(pendingRent.value)+Number(fixedRent.value); 
+  paidAmount.setAttribute("placeholder","Total: "+total);
+  
+   
+
+  
