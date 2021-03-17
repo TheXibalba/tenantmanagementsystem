666b1d4a9678ac42117aa0f52f23be4c29c82656
Yuvraj Pawar <yurajpawar99@gmail.com>
2021-03-17 22:31:31 +0530

    minor commit

--- app.js
+++ app.js
@@ -33,19 +33,19 @@ const tmsAdminSchema = new mongoose.Schema({
     password: String
 });
 
-/* const Admin = new mongoose.model("admin", tmsAdminSchema);
+const Admin = new mongoose.model("admin", tmsAdminSchema);
 const newAdmin = new Admin({
     username: process.env.EMAIL_ID,
     password: process.env.PASSWORD
-}); */
+});
 
- newAdmin.save(function(err){
+/*  newAdmin.save(function(err){
  if(err){
      console.log(err);
  }else{
      console.log("admin has been created successfully!");
  }
-});   
+});    */
 
 
 app.use(bodyParser.urlencoded({
